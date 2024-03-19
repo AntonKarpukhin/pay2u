38fc3e01cb2b551ff072a82123113daf4f3c2f4c
Your Name <you@example.com>
2024-03-19 14:04:23 +0300

    fix5/onboarding page.

--- src/page/app/app.tsx
+++ src/page/app/app.tsx
@@ -1,19 +1,25 @@
-
-import styles from './app.module.css';
 import OnboardingPage from "../onboarding-page/onboarding-page.tsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "../main-page/main-page.tsx";
+import styles from './app.module.css';
+import cn from "classnames";
+
 
 function App() {
 
-  return (
-    <main className={styles.main}>
-		<Routes>
-			<Route path={'/'} element={<MainPage/>}/>
-			<Route path={'/onboarding'} element={<OnboardingPage/>}/>
-		</Routes>
-    </main>
-  )
+	const location = useLocation();
+	const onboarding = location.pathname.includes('onboarding');
+
+	return (
+		<main className={cn(styles.main, {
+			[styles.mainOnboarding]: onboarding
+		})}>
+			<Routes>
+				<Route path={'/'} element={<MainPage/>}/>
+				<Route path={'/onboarding'} element={<OnboardingPage/>}/>
+			</Routes>
+		</main>
+	)
 }
 
 export default App
