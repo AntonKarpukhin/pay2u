8aee65d2d3a506608b78b88d4f0268a1c55256af
Your Name <you@example.com>
2024-03-19 14:54:57 +0300

    fix6/onboarding page.

--- src/components/onboarding-item/onboarding-item.tsx
+++ src/components/onboarding-item/onboarding-item.tsx
@@ -2,10 +2,14 @@ import { OnboardingItemProps } from "./onboarding-item.props.ts";
 import styles from './onboarding-item.module.css';
 
 const OnboardingItem = ({title, subtitle, img}: OnboardingItemProps) => {
+
+	const title1 = title.includes('Управление подписками') ? title.slice(0, 23) : title;
+	const title2 = title.includes('Управление подписками') ? title.slice(23) : null;
+
 	return (
 		<div className={styles.OnboardingItem}>
 			<div className={styles.wrapper}>
-				<p className={ styles.title }>{ title }</p>
+				<p className={ styles.title}>{ title1 } { title1 ? <br/> : null } <span>{title2 ? title2 : null}</span></p>
 				<p className={ styles.subtitle }>{ subtitle }</p>
 			</div>
 			<img className={ styles.img } src={ img } alt='Подарки'/>
